32ba07b0de006a394a278a7e3ac0b128ae4e23fb
DmitryGLHF <dima.rize@yandex.ru>
2024-04-07 19:53:00 +0300

    Initial commit

---
+++ Topomatic.LookUp/Topomatic.LookUp/Module.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Topomatic.ApplicationPlatform.Plugins;
+using Topomatic.Controls.Dialogs;
+
+namespace Topomatic.LookUp
+{
+    partial class Module : Topomatic.ApplicationPlatform.Plugins.PluginInitializator
+    {
+        [cmd("lookup_cmd")]
+        public void LookUpFunction()
+        {
+            MessageDlg.Show("Hello world");
+        }
+    }
+}
---
+++ Topomatic.LookUp/Topomatic.LookUp/ModulePluginHost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Topomatic.LookUp
+{
+    public class ModulePluginHost : Topomatic.ApplicationPlatform.Plugins.PluginHostInitializator
+    {
+        protected override Type[] GetTypes()
+        {
+            return new Type[] { typeof(Module) };
+        }
+    }
+}
